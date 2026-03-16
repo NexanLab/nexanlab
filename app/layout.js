b87605a062a04423d1f7e15ebbc7d954dc79d26b
fatih <oranmuhammedfatih@gmail.com>
2026-03-16 12:20:58 +0300

    fix google verification

--- app/layout.js
+++ app/layout.js
@@ -84,6 +84,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="ooLbsxAGnEbLQlg_Sfd07nKPRz9T3gPCbKxANONWTy0" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
